2b06e36867a049b8e1dda15df18a0563fbae4328
Hanson <105476@grundfos.com>
2023-05-19 11:57:09 +0800

    init training-report page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import { useState } from "react";
 import { Outlet } from "react-router-dom";
+import "./styles/css/App.css";
+import Button from "@mui/joy/Button";
+import Menu from "@mui/joy/Menu";
+import MenuItem from "@mui/joy/MenuItem";
 
 import { ReactComponent as DarkSvg } from "./svg/sun.svg";
 import { ReactComponent as LightSvg } from "./svg/sun-light.svg";
@@ -10,6 +14,22 @@ function App() {
 	return (
 		<div className={`App ${theme}`}>
 			<Outlet />
+
+			<div className="nav">
+				<Button
+					// id="basic-demo-button"
+					// aria-controls={ "basic-menu" }
+					// aria-haspopup="true"
+					// aria-expanded={ "true" }
+					// variant="outlined"
+					// color="neutral"
+					onClick={() => {
+						settheme(theme === "dark" ? "light" : "dark");
+					}}
+				>
+					{theme === "dark" ? <LightSvg /> : <DarkSvg />}
+				</Button>
+			</div>
 		</div>
 	);
 }
