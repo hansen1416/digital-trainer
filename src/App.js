5c06c8a8a62e6f0b190d1d4b59981d180bc67b57
hansen1416 <badapplesweetie@gmail.com>
2023-05-19 22:22:17 +0800

    1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { useState } from "react";
 import { Outlet } from "react-router-dom";
 import "./styles/css/App.css";
-import Button from "@mui/joy/Button";
-import Menu from "@mui/joy/Menu";
-import MenuItem from "@mui/joy/MenuItem";
+import List from "@mui/joy/List";
+import ListItem from "@mui/joy/ListItem";
+import ListItemButton from "@mui/joy/ListItemButton";
+import ListDivider from "@mui/joy/ListDivider";
+import ListItemDecorator from "@mui/joy/ListItemDecorator";
 
 import { ReactComponent as DarkSvg } from "./svg/sun.svg";
 import { ReactComponent as LightSvg } from "./svg/sun-light.svg";
@@ -16,26 +18,60 @@ function App() {
 			<Outlet />
 
 			<div className="nav">
-				<div className="menu">
-					<Menu>
-						<MenuItem>Report</MenuItem>
-					</Menu>
-				</div>
-				<div className="controls">
-					<Button
-						// id="basic-demo-button"
-						// aria-controls={ "basic-menu" }
-						// aria-haspopup="true"
-						// aria-expanded={ "true" }
-						// variant="outlined"
-						// color="neutral"
-						onClick={() => {
-							settheme(theme === "dark" ? "light" : "dark");
-						}}
-					>
-						{theme === "dark" ? <LightSvg /> : <DarkSvg />}
-					</Button>
-				</div>
+				<List
+					role="menubar"
+					orientation="horizontal"
+					sx={{
+						display: "flex",
+						flexDirection: "row",
+						justifyContent: "flex-end",
+					}}
+				>
+					<ListItem role="none">
+						<ListItemButton
+							role="menuitem"
+							component="a"
+							href="#horizontal-list"
+						>
+							Digital Trainer
+						</ListItemButton>
+					</ListItem>
+					<ListDivider />
+					<ListItem role="none">
+						<ListItemButton
+							role="menuitem"
+							component="a"
+							href="#horizontal-list"
+						>
+							Training Builder
+						</ListItemButton>
+					</ListItem>
+					<ListDivider />
+					<ListItem role="none">
+						<ListItemButton
+							role="menuitem"
+							component="a"
+							href="#horizontal-list"
+							aria-label="Profile"
+						>
+							Training Explorer
+						</ListItemButton>
+					</ListItem>
+					<ListDivider />
+					<ListItem role="none">
+						<ListItemButton
+							role="menuitem"
+							component="a"
+							href="#horizontal-list"
+							aria-label="Profile"
+							onClick={() => {
+								settheme(theme === "dark" ? "light" : "dark");
+							}}
+						>
+							{theme === "dark" ? <LightSvg /> : <DarkSvg />}
+						</ListItemButton>
+					</ListItem>
+				</List>
 			</div>
 		</div>
 	);
