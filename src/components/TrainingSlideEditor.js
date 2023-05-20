ad7887d2b3ff2e61fbebbaa554b0dab571d72605
hansen1416 <badapplesweetie@gmail.com>
2023-05-20 19:06:42 +0800

    training builder

--- src/components/TrainingSlideEditor.js
+++ src/components/TrainingSlideEditor.js
@@ -26,7 +26,146 @@ export default function TrainingSlideEditor({ trainingData, settrainingData }) {
 
 	return (
 		<div className="training-slide-editor" ref={kasten}>
-			{trainingData && trainingData.name && <section></section>}
+			{trainingData && trainingData.name && (
+				<section>
+					<div className="title">
+						<div>
+							<div className="name">
+								<span>
+									name:{" "}
+									<input
+										value={trainingData.name}
+										onChange={(e) => {
+											const tmp = cloneDeep(trainingData);
+
+											tmp.name = e.target.value;
+
+											settrainingData(tmp);
+										}}
+									/>
+								</span>
+							</div>
+							<div className="stats">
+								<span>
+									duration:{" "}
+									{roundToTwo(trainingData.duration)}
+								</span>
+								<span>
+									intensity:{" "}
+									{roundToTwo(trainingData.intensity)}
+								</span>
+								<span>
+									calories:{" "}
+									{roundToTwo(trainingData.calories)}
+								</span>
+							</div>
+							<div>
+								<MusclePercentage
+									musclesPercent={trainingData.muscle_groups}
+								/>
+							</div>
+						</div>
+						<div className="operation">
+							<Button
+								variant="primary"
+								onClick={() => {
+									// todo, save to API
+									window.localStorage.setItem(
+										"mytraining",
+										JSON.stringify(trainingData)
+									);
+
+									alert("Training Saved, let's try it!");
+								}}
+							>
+								Save to my list
+							</Button>
+						</div>
+					</div>
+					<Splide
+						options={{
+							type: "slide",
+							focus: 0,
+							perMove: 1,
+							fixedWidth: itemWidth,
+							// fixedHeight: 200,
+							gap: 10,
+							arrows: false,
+							rewind: true,
+							pagination: false,
+						}}
+					>
+						{Boolean(trainingData && trainingData.exercises) &&
+							trainingData.exercises.map((exercise, idx) => {
+								return (
+									<SplideSlide key={idx}>
+										<div
+											style={{
+												width: itemWidth,
+												height: "100%",
+											}}
+										>
+											<div
+												style={{
+													width: itemWidth,
+													height: itemWidth,
+												}}
+											>
+												<img
+													style={{
+														width: "100%",
+														height: "100%",
+													}}
+													src={
+														process.env.PUBLIC_URL +
+														"/data/imgs/" +
+														exercise.name +
+														".png"
+													}
+													alt=""
+												/>
+											</div>
+											<div>
+												<div className="num-control">
+													<span>reps: </span>
+													<span>
+														<InputIncreaseDecrease
+															value={
+																exercise.reps
+															}
+															onChange={(v) => {
+																updateExercise(
+																	idx,
+																	{ reps: v }
+																);
+															}}
+														/>
+													</span>
+												</div>
+												<div className="num-control">
+													<span>rest: </span>
+													<span>
+														<InputIncreaseDecrease
+															value={
+																exercise.rest
+															}
+															onChange={(v) => {
+																updateExercise(
+																	idx,
+																	{ rest: v }
+																);
+															}}
+														/>
+													</span>
+												</div>
+											</div>
+										</div>
+									</SplideSlide>
+								);
+							})}
+					</Splide>
+				</section>
+			)}
 		</div>
 	);
 }
