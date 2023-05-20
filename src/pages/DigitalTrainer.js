bf43bbd62332c508bb5cfba93d879c1841254dac
hansen1416 <badapplesweetie@gmail.com>
2023-05-20 23:10:40 +0800

    main scene

--- src/pages/DigitalTrainer.js
+++ src/pages/DigitalTrainer.js
@@ -1,11 +1,35 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 
 export default function DigitalTrainer() {
+	const canvasRef = useRef(null);
+	const scene = useRef(null);
+	const camera = useRef(null);
+	const renderer = useRef(null);
+	const controls = useRef(null);
+	// an integer number, used for cancelAnimationFrame
+	const animationPointer = useRef(0);
+	const counter = useRef(0);
+
+	const videoRef = useRef(null);
+
+	const [subsceneWidth, setsubsceneWidth] = useState(0);
+	const [subsceneHeight, setsubsceneHeight] = useState(0);
+
 	useEffect(() => {}, []);
 
 	return (
 		<div className="digital-trainer">
-			<h1>d trainer</h1>
+			<video
+				ref={videoRef}
+				autoPlay={true}
+				width={subsceneWidth + "px"}
+				height={subsceneHeight + "px"}
+				style={{
+					display: "none",
+				}}
+			></video>
+
+			<canvas ref={canvasRef} />
 		</div>
 	);
 }
