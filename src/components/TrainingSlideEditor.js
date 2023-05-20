b45b0e9cffeb7c3b96ba9a9cd864a7e01449e62e
hansen1416 <badapplesweetie@gmail.com>
2023-05-20 19:06:08 +0800

    training builder

--- src/components/TrainingSlideEditor.js
+++ src/components/TrainingSlideEditor.js
@@ -2,6 +2,28 @@ import { useEffect, useRef, useState } from "react";
 import { Splide, SplideSlide } from "@splidejs/react-splide";
 
 export default function TrainingSlideEditor({ trainingData, settrainingData }) {
+	const kasten = useRef(null);
+
+	const [itemWidth, setitemWidth] = useState(0);
+	const trainingDataRef = useRef(null);
+
+	useEffect(() => {
+		let resizeObserver;
+		// watch box size change and set size for individual block
+		if (kasten.current) {
+			// wait for the elementRef to be available
+			resizeObserver = new ResizeObserver(([ResizeObserverEntry]) => {
+				// Do what you want to do when the size of the element changes
+				const width = parseInt(
+					ResizeObserverEntry.contentRect.width / 6
+				);
+
+				setitemWidth(width);
+			});
+			resizeObserver.observe(kasten.current);
+		}
+	}, []);
+
 	return (
 		<div className="training-slide-editor" ref={kasten}>
 			{trainingData && trainingData.name && <section></section>}
