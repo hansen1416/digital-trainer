446bc60b71ee1d88e2f4f5de6c5d35dcc55bc685
hansen1416 <badapplesweetie@gmail.com>
2023-05-21 10:48:31 +0800

    main scene

--- src/lib/ropes.js
+++ src/lib/ropes.js
@@ -330,31 +330,6 @@ export function isLowerBodyVisible(poseData) {
 	);
 }
 
-export function pearson_corr(x, y) {
-	let sumX = 0,
-		sumY = 0,
-		sumXY = 0,
-		sumX2 = 0,
-		sumY2 = 0;
-	const minLength = (x.length = y.length = Math.min(x.length, y.length)),
-		reduce = (xi, idx) => {
-			const yi = y[idx];
-			sumX += xi;
-			sumY += yi;
-			sumXY += xi * yi;
-			sumX2 += xi * xi;
-			sumY2 += yi * yi;
-		};
-	x.forEach(reduce);
-	return (
-		(minLength * sumXY - sumX * sumY) /
-		Math.sqrt(
-			(minLength * sumX2 - sumX * sumX) *
-				(minLength * sumY2 - sumY * sumY)
-		)
-	);
-}
-
 export function array_average(array) {
 	return array.reduce((a, b) => a + b) / array.length;
 }
