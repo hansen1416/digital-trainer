c2a4514ca31415e4f54915d9aa2f311e9bb9b8bc
hansen1416 <badapplesweetie@gmail.com>
2023-05-21 10:37:14 +0800

    main scene

--- src/lib/ropes.js
+++ src/lib/ropes.js
@@ -252,3 +252,31 @@ export function radianGradientColor(radian) {
 		return [68, 170, 136];
 	}
 }
+
+export function jsonToBufferGeometry(data) {
+	const geometry = new THREE.BufferGeometry();
+
+	geometry.setAttribute(
+		"position",
+		new THREE.BufferAttribute(
+			new Float32Array(data.data.attributes.position.array),
+			3
+		)
+	);
+	geometry.setAttribute(
+		"normal",
+		new THREE.BufferAttribute(
+			new Float32Array(data.data.attributes.normal.array),
+			3
+		)
+	);
+	geometry.setAttribute(
+		"uv",
+		new THREE.BufferAttribute(
+			new Float32Array(data.data.attributes.uv.array),
+			2
+		)
+	);
+
+	return geometry;
+}
